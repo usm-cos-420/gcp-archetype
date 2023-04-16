17dd81453eed80db01de002c8049a40e7ca6a9a8
Bruce MacLeod <macleod@maine.edu>
2023-04-16 06:33:17 -0400

    Create createBlogPost.java
    Second step from Form Data Tutorial : https://cloud.google.com/appengine/docs/legacy/standard/java/building-app/handling-form-data

---
+++ src/main/java/edu/usm/cos420/Servlets/createBlogPost.java
@@ -0,0 +1,25 @@
+package edu.usm.cos420.Servlets;
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet(name = "createBlogPost", value="/create")
+public class createBlogPost extends HttpServlet {
+
+  @Override
+  public void doPost(HttpServletRequest req, HttpServletResponse resp)
+      throws ServletException, IOException {
+
+    PrintWriter out = resp.getWriter();
+
+    out.println(
+        "Article with the title: " + req.getParameter("title") + " by "
+            + req.getParameter("author") + " and the content: "
+            + req.getParameter("description") + " added.");
+  }
+}
