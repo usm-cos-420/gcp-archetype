bc8c1591db1c4171306088406e3634dc3c76a242
Bruce MacLeod <macleod@maine.edu>
2023-04-19 06:18:11 -0400

    Create a new domain class to hold Blog Post entries

---
+++ src/main/java/edu/usm/cos420/domain/BlogPost.java
@@ -0,0 +1,31 @@
+package edu.usm.cos420.domain;
+
+public class BlogPost {
+	private String title;
+	private String author;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
+	private String description;
+	
+	public String toString() {
+		return "Title: "
+				+  this.title + "\nAuthor: " + this.author + "\nDesciption: " + this.description;
+	}
+
+}
